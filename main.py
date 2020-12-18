582a090899f0bbcf0a5473fc605e913dd9f61f40
yoonhero <yoonhero06@naver.com>
2020-12-18 23:18:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from babel.numbers import format_currency
+
+os.system("clear")
+
+"""
+Use the 'format_currency' function to format the output of the conversion
+format_currency(AMOUNT, CURRENCY_CODE, locale="ko_KR" (no need to change this one))
+"""
+
+
+
+print(format_currency(5000, "KRW", locale="ko_KR"))
